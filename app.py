77f6fe105ec22f8c1540e80f6a665393ae658f54
motoki <krkrmfmf@outlook.jp>
2022-07-13 00:09:13 +0900

    再生機能を追加

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
+from asyncio import subprocess
+from curses.ascii import NUL
 from flask import Flask
+import subprocess
+import psutil
 
 app = Flask(__name__)
 
+# サブプロセス
+proc = None
+
+
 @app.route("/")
 def hello_world():
+    proc = subprocess.Popen(
+        ["omxplayer", "/home/motoki/idol.mp4"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+
+    print(proc)
+
     return "<p>Hello, World!</p>"
+
+
+@app.route("/stop")
+def stop():
+    pid = "not"
+
+    pids = {
+        p.info["name"]: p.info["pid"]
+        for p in psutil.process_iter(attrs=["pid", "name"])
+    }
+
+    if ('omxplayer.bin' in pids):
+        pid = pids['omxplayer.bin']
+        p = psutil.Process(pid)
+        p.terminate()
+
+    ret = {"pid": pid}
+
+    return ret
