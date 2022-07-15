60842efb559a89f1f61c2c97c500aef4ac95aefc
motoki <krkrmfmf@outlook.jp>
2022-07-16 03:54:38 +0900

    UIの実装

--- static/main.js
+++ static/main.js
@@ -1,6 +1,21 @@
 const main = () => {
     getMovies();
+    openUploadModalButton();
+    modalCloseButton();
+    stopButton();
+    sendButton();
+    bodyClick();
+};
 
+const openUploadModalButton = () => {
+    const openUploadModalButtonElement = document.getElementById('upload-modal-open-button');
+    openUploadModalButtonElement.addEventListener('click', () => {
+        const uploadModalElement = document.getElementById('upload-modal');
+        uploadModalElement.style.display = 'block';
+    });
+};
+
+const stopButton = () => {
     const stopButtonElement = document.getElementById('stop');
     stopButtonElement.addEventListener('click', (event) => {
         event.preventDefault();
@@ -20,6 +35,36 @@ const main = () => {
                 console.error(reson);
             });
     });
+};
+
+const getMovies = () => {
+    fetch('/list')
+        .then((response) => {
+            response
+                .json()
+                .then((data) => {
+                    const movieListElement = document.getElementById('movie-list');
+                    while (movieListElement.firstChild) {
+                        movieListElement.removeChild(movieListElement.firstChild);
+                    }
+
+                    data.files.forEach((record) => {
+                        const linkElement = document.createElement('a');
+
+                        linkElement.innerText = record;
+                        linkElement.href = `/play/${record}`;
+                        linkElement.addEventListener('click', requestMoviePlay);
+
+                        movieListElement.appendChild(linkElement);
+                    });
+                })
+                .catch((reson) => {
+                    console.error(reson);
+                });
+        })
+        .catch((reson) => {
+            console.error(reson);
+        });
 };
 
 const requestMoviePlay = (event) => {
@@ -42,35 +87,56 @@ const requestMoviePlay = (event) => {
         });
 };
 
-const getMovies = () => {
-    fetch('/list')
-        .then((response) => {
-            response
-                .json()
-                .then((data) => {
-                    const ulElement = document.createElement('ul');
-                    data.files.forEach((record) => {
-                        const liElement = document.createElement('li');
-                        const linkElement = document.createElement('a');
+const sendButton = () => {
+    const sendButtonElement = document.getElementById('upload-button');
+    sendButtonElement.addEventListener('click', fileUpload);
+};
 
-                        linkElement.innerText = record;
-                        linkElement.href = `/play/${record}`;
-                        linkElement.addEventListener('click', requestMoviePlay);
-                        liElement.appendChild(linkElement);
+const fileUpload = () => {
+    const uploadProgressElement = document.getElementById('upload-progress');
+    const selectFileElement = document.getElementById('select-file');
 
-                        ulElement.appendChild(liElement);
-                    });
+    if (selectFileElement.files[0] != undefined) {
+        uploadProgressElement.value = 0;
+        uploadProgressElement.max = selectFileElement.files[0].size;
+    }
 
-                    const movieListElement = document.getElementById('movie-list');
-                    movieListElement.appendChild(ulElement);
-                })
-                .catch((reson) => {
-                    console.error(reson);
-                });
-        })
-        .catch((reson) => {
-            console.error(reson);
-        });
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('POST', '/upload', true);
+
+    xhr.upload.onprogress = (e) => {
+        uploadProgressElement.value = e.loaded;
+        console.log(`Upload ${e.loaded} / ${e.total}`);
+    };
+
+    xhr.upload.onload = () => {
+        getMovies();
+
+        modalClose();
+    };
+
+    const uploadForm = document.getElementById('upload-form');
+    const formData = new FormData(uploadForm);
+    xhr.send(formData);
+};
+
+const modalCloseButton = () => {
+    const modalCloseButtonElement = document.getElementById('modal-close-button');
+    modalCloseButtonElement.addEventListener('click', () => {
+        modalClose();
+    });
+};
+
+const modalClose = () => {
+    const uploadModalElement = document.getElementById('upload-modal');
+
+    uploadModalElement.classList.add('modal-close');
+
+    setTimeout(() => {
+        uploadModalElement.style.display = 'none';
+        uploadModalElement.classList.remove('modal-close');
+    }, 1000);
 };
 
 main();
